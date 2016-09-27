09986236e231a9ff73dc3bfff87d9028dc9460f4
isapozhkov <i.sapozhkov.93@gmail.com>
2016-09-27 16:39:40 +0300

    Update setup and make

---
+++ Makefile
@@ -0,0 +1,10 @@
+install:
+	python setup.py install
+
+package:
+	python setup.py clean
+	python setup.py sdist
+
+clean:
+	rm -rf build
+	rm -rf dist/
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='ntripbrowser',
+    version='0.0.1',
+    author='Ivan Sapozhkov',
+    author_email='ivan.sapozhkov@emlid.com',
+    py_modules=['ntripbrowser'],
+    install_requires=['texttable'],
+    license = 'GPLv3',
+    url='https://github.com/emlid/ntripbrowser.git',
+    description='NTRIP Browser for terminal',
+    long_description=open('README.md').read(),
+)
