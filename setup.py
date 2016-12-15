c0ada053195060685b3358ffbc0887d27bd1d0a2
isapozhkov <i.sapozhkov.93@gmail.com>
2016-12-15 20:18:10 +0300

    New release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ntripbrowser',
-    version='0.0.4',
+    version='0.0.5',
     author='Ivan Sapozhkov',
     author_email='ivan.sapozhkov@emlid.com',
     py_modules=['ntripbrowser'],
