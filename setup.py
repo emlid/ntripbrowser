55ca4b59a841bb0294d41901159eb96af90699e6
andrewyushkevich <andrew.yushkevich@emlid.com>
2017-06-22 14:50:22 +0300

    new texttable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ntripbrowser',
-    version='1.1.0',
+    version='1.1.1',
     author='Andrew Yushkevich',
     author_email='andrew.yushkevich@emlid.com',
     packages=['ntripbrowser'],
