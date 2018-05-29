27b2abf9630f14ab74cba99f62987477a38ebc2b
yashin-alexander <alexandr.yashin@emlid.com>
2018-05-29 20:21:15 +0300

    update setup.py
     - tox.ini added

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     author='Andrew Yushkevich',
     author_email='andrew.yushkevich@emlid.com',
     packages=['ntripbrowser'],
-    install_requires=['chardet', 'geopy','texttable'],
-    license = 'BSD-3-Clause',
+    install_requires=['chardet', 'geopy', 'texttable', 'pager', 'pycurl'],
+    tests_requires=['pytest', 'mock', 'tox'],
+    license='BSD-3-Clause',
     url='https://github.com/emlid/ntripbrowser.git',
-    description='NTRIP Browser for terminal',
-    long_description=open('README.md').read(),
-    entry_points = {
+    description='NTRIP cli browser',
+    entry_points={
         'console_scripts': [
-            'ntripbrowser = ntripbrowser.ntripbrowser:main',
+            'ntripbrowser = ntripbrowser.browser:main',
         ],
     },
 )
