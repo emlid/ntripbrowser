32a7ceb65b199e92c6f2832caab002745ea1bafd
yashin-alexander <alexandr.yashin@emlid.com>
2018-08-01 15:55:23 +0300

    ntripbrowser/__init__.py: code style fixes

--- ntripbrowser/__init__.py
+++ ntripbrowser/__init__.py
@@ -2,3 +2,7 @@ from .ntripbrowser import NtripBrowser
 from .exceptions import (NtripbrowserError, ExceededTimeoutError, NoDataFoundOnPage,
                          NoDataReceivedFromCaster, UnableToConnect, HandshakeFiledError)
 from .constants import STR_HEADERS, NET_HEADERS, CAS_HEADERS
+
+__all__ = ['NtripBrowser', 'NtripbrowserError', 'ExceededTimeoutError',
+           'NoDataFoundOnPage', 'NoDataReceivedFromCaster', 'UnableToConnect', 'HandshakeFiledError',
+           'STR_HEADERS', 'NET_HEADERS', 'CAS_HEADERS']
