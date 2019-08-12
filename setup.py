6c9704385e7905834bc1369f42eb34f684028375
Alexander Yashin <alexandr.yashin@emlid.com>
2019-08-12 15:32:19 +0300

    setup.py: Bump version to v2.2.0
    Use `cchardet` instead of `chardet`.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='ntripbrowser',
-    version='2.1.0',
+    version='2.2.0',
     author='Andrew Yushkevich, Alexander Yashin',
     author_email='andrew.yushkevich@emlid.com, alexandr.yashin@emlid.com',
     packages=['ntripbrowser'],
-    install_requires=['chardet', 'geopy>=1.14', 'texttable', 'pager', 'pycurl', 'cachecontrol>=0.12.4'],
+    install_requires=['cchardet>=2.1.4', 'geopy>=1.14', 'texttable', 'pager', 'pycurl', 'cachecontrol>=0.12.4'],
     tests_requires=['pytest', 'mock', 'tox'],
     license='BSD-3-Clause',
     url='https://github.com/emlid/ntripbrowser.git',
