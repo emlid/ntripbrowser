68647173fcc21582bacab50ea75d6642332091b0
Alexander Yashin <alexandr.yashin@emlid.com>
2019-08-15 15:18:15 +0300

    ntripbrowser: get rid of unused exceptions
    Some of the exception classes mess up the code too much.
    Fix typo in `HandshakeFailedError` class name.

--- ntripbrowser/__init__.py
+++ ntripbrowser/__init__.py
@@ -1,8 +1,8 @@
 from .ntripbrowser import NtripBrowser
-from .exceptions import (NtripbrowserError, ExceededTimeoutError, NoDataFoundOnPage,
-                         NoDataReceivedFromCaster, UnableToConnect, HandshakeFiledError)
+from .exceptions import (NtripbrowserError, ExceededTimeoutError, NoDataReceivedFromCaster,
+                         UnableToConnect)
 from .constants import STR_HEADERS, NET_HEADERS, CAS_HEADERS
 
 __all__ = ['NtripBrowser', 'NtripbrowserError', 'ExceededTimeoutError',
-           'NoDataFoundOnPage', 'NoDataReceivedFromCaster', 'UnableToConnect', 'HandshakeFiledError',
+           'NoDataReceivedFromCaster', 'UnableToConnect',
            'STR_HEADERS', 'NET_HEADERS', 'CAS_HEADERS']
