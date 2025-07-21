c976a3b7bcce050a146c136ba6baac4b2eaf1074
Viktor Vorobev <viktor.vorobev@emlid.com>
2025-07-21 14:13:43 +0200

    ntripbrowser: fix format errors and drop python2

--- ntripbrowser/__init__.py
+++ ntripbrowser/__init__.py
@@ -1,8 +1,14 @@
+from .constants import CAS_HEADERS, NET_HEADERS, STR_HEADERS
+from .exceptions import ExceededTimeoutError, NoDataReceivedFromCaster, NtripbrowserError, UnableToConnect
 from .ntripbrowser import NtripBrowser
-from .exceptions import (NtripbrowserError, ExceededTimeoutError, NoDataReceivedFromCaster,
-                         UnableToConnect)
-from .constants import STR_HEADERS, NET_HEADERS, CAS_HEADERS
 
-__all__ = ['NtripBrowser', 'NtripbrowserError', 'ExceededTimeoutError',
-           'NoDataReceivedFromCaster', 'UnableToConnect',
-           'STR_HEADERS', 'NET_HEADERS', 'CAS_HEADERS']
+__all__ = [
+    "NtripBrowser",
+    "NtripbrowserError",
+    "ExceededTimeoutError",
+    "NoDataReceivedFromCaster",
+    "UnableToConnect",
+    "STR_HEADERS",
+    "NET_HEADERS",
+    "CAS_HEADERS",
+]
